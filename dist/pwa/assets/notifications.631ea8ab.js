9c7258ef07c9f345a01c1c660dfa23f6c425a5b9
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-07-07 17:09:24 +0000

    committing dist/pwa

---
+++ dist/pwa/assets/notifications.631ea8ab.js
@@ -0,0 +1,1 @@
+import{N as o}from"./index.e45af843.js";o.setDefaults({position:"bottom-right",color:"primary",textColor:"white",timeout:{}.ENVIRONMENT==="local"?0:3e3,actions:[{label:"close",color:"white"}]});
---
+++ dist/pwa/assets/use-quasar.78a54e71.js
@@ -0,0 +1,1 @@
+import{i as a,a6 as r}from"./index.e45af843.js";function u(){return a(r)}export{u};
