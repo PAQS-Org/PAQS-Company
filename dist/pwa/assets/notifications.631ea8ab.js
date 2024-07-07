4222ac92ae49109350b0443f62bed59fb7592a04
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-07-07 17:13:57 +0000

    remove dist

--- dist/pwa/assets/notifications.631ea8ab.js
+++
@@ -1,1 +0,0 @@
-import{N as o}from"./index.e45af843.js";o.setDefaults({position:"bottom-right",color:"primary",textColor:"white",timeout:{}.ENVIRONMENT==="local"?0:3e3,actions:[{label:"close",color:"white"}]});
