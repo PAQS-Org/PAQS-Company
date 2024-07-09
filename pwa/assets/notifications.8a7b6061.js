1fafad8c47b0563fc3f0b724e35eca4151abc692
osahene <bra.kwameadu3@gmail.com>
2024-07-09 01:02:18 +0000

    add dist

---
+++ pwa/assets/notifications.8a7b6061.js
@@ -0,0 +1,1 @@
+import{N as o}from"./index.92c4a599.js";o.setDefaults({position:"bottom-right",color:"primary",textColor:"white",timeout:{}.ENVIRONMENT==="local"?0:3e3,actions:[{label:"close",color:"white"}]});
--- src/boot/notify.js
+++
@@ -1,5 +0,0 @@
-import { Notify } from 'quasar';
-
-export default ({ app }) => {
-  app.config.globalProperties.$notify = Notify.create;
-};
