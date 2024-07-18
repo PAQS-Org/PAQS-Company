7d676d87e7f651196b9dc343b27cb5533d4566d2
osahene <bra.kwameadu3@gmail.com>
2024-07-18 05:45:34 +0000

    updated the header stats

--- src/components/Headers/HeaderStats.vue
+++ src/components/Headers/HeaderStats.vue
@@ -5,35 +5,35 @@
       <div>
         <!-- Card stats -->
         <div class="flex flex-wrap">
-          <div class="w-full lg:w-6/12 xl:w-3/12 px-4" v-if="totalScanComplete">
+          <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
             <card-stats
               stat-subtitle="TOTAL CHECKOUT"
               :stat-title="totalScanComplete.totalForMonth"
-              stat-arrow="../../../src/assets/img/svg/avg.svg"
-              :stat-percent="totalScanComplete.averageForYTD"
+              stat-arrow="fas fa-chart-line fa-fade"
+              :stat-percent="`${totalScanComplete.averageForYTD} %`"
               stat-percent-color="text-emerald-500"
               :stat-descripiron="`Year-to-Date (YTD) for the month of ${currentMonth}`"
               stat-icon-name="fas fa-bolt"
               icon-color="color:#3e92d1"
             />
           </div>
-          <div class="w-full lg:w-6/12 xl:w-3/12 px-4" v-if="totalScan">
+          <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
             <card-stats
               stat-subtitle="TOTAL SCAN"
               :stat-title="totalScan.totalForMonth"
-              stat-arrow="../../../src/assets/img/svg/avg.svg"
-              :stat-percent="totalScan.averageForYTD"
+              stat-arrow="fas fa-chart-line fa-fade"
+              :stat-percent="`${totalScan.averageForYTD} %`"
               stat-percent-color="text-red-500"
               :stat-descripiron="`Year-to-Date (YTD) for the month of ${currentMonth}`"
               stat-icon-name="fas fa-qrcode"
               icon-color="color:#e59400"
             />
           </div>
-          <div class="w-full lg:w-6/12 xl:w-3/12 px-4" v-if="topLocation">
+          <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
             <card-stats
               stat-subtitle="TOP LOCATION"
               :stat-title2="topLocation.location"
-              stat-arrow="../../../src/assets/img/svg/cons.svg"
+              stat-arrow="far fa-face-kiss-wink-heart fa-fade"
               :stat-complete="`Checkout: ${topLocation.completed}`"
               :stat-scan="`Scans: ${topLocation.scanned}`"
               :stat-percent="`${topLocation.conversionRate} %`"
@@ -47,7 +47,7 @@
             <card-stats
               stat-subtitle="GENERAL PERFORMANCE"
               :stat-title="genPerformance.bestYear.percentage"
-              stat-arrow="fas fa-bullseye"
+              stat-arrow="fas fa-bullseye fa-fade"
               :stat-percent="genPerformance.bestMonth.count"
               stat-percent-color="text-emerald-500"
               :stat-descripiron="`Best performing moment: ${genPerformance.bestMonth.monthYear}`"
